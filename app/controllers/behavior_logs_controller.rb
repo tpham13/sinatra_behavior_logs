3791be2e1d99a15999b8b003e70ae9f13b2ddea3
tpham13 <tpham0123@gmail.com>
2020-08-21 16:24:56 -0700

    added behavior_log/index

--- app/controllers/behavior_logs_controller.rb
+++ app/controllers/behavior_logs_controller.rb
@@ -2,11 +2,17 @@ class BehaviorLogsController < ApplicationController
 
     get '/behavior_logs' do 
         if logged_in?
-            @logs = BehaviorLog.all
+            @logs = current_user.behavior_logs
+            
             erb :'/behavior_logs/index'    
         else
             redirect '/login'
         end 
+    end 
+
+    get '/behavior_logs/:id' do 
+        @log = @logs.find_by(:id)
+        erb :'/behavior_logs/show'
     end 
             
         
