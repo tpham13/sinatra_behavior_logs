1467456320cc44b9ac2087a46c0c87fe99417f7e
tpham13 <tpham0123@gmail.com>
2020-08-22 18:23:20 -0700

    created behavior_logs/new

--- app/controllers/behavior_logs_controller.rb
+++ app/controllers/behavior_logs_controller.rb
@@ -1,5 +1,6 @@
-class BehaviorLogsController < ApplicationController
 
+class BehaviorLogsController < ApplicationController
+    require 'pry'
     get '/behavior_logs' do 
         if logged_in?
             @logs = current_user.behavior_logs
@@ -10,10 +11,21 @@ class BehaviorLogsController < ApplicationController
         end 
     end 
 
+    get '/behavior_logs/new' do 
+        erb :'behavior_logs/new'
+    end 
+
+    post '/behavior_logs/new' do 
+        log = current_user.behavior_logs.build(params)
+        log.save
+        redirect '/behavior_logs' #could use erb to index here but you won't see the new behavior log
+        
+    end 
+    
     get '/behavior_logs/:id' do 
-        @log = @logs.find_by(:id)
+        @post.user == current_user
         erb :'/behavior_logs/show'
     end 
-            
+          
         
 end 
